f21caada7e5529d300e53c3eaba62296c269ed27
Dast1 <myl90@126.com>
2020-03-11 10:01:51 +0800

    使用BehaviorSubject

--- RxSwiftBiliBili/RxSwiftBiliBili/Subject/SubjectVC.swift
+++ RxSwiftBiliBili/RxSwiftBiliBili/Subject/SubjectVC.swift
@@ -17,11 +17,14 @@ class SubjectVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //无需初始值的Subject
-        testPublishSub()
+        //无需初始值的Subject:类似直播
+//        testPublishSub()
         
-        //todo:需要初始值的Subject
-        testBehaviorSub()
+        //需要初始值的Subject:订阅时可以收到订阅前上一次发送的消息（默认值底层也是消息，上一次无发送消息时，会收到默认值的消息）；
+//        testBehaviorSub()
+        
+         //如果源 Observable 因为产生了一个 error 事件而中止， BehaviorSubject 就不会发出任何元素，而是将这个 error 事件发送出来。
+        testBehaviorSubError()
     }
     
     func testPublishSub() {
@@ -84,7 +87,124 @@ class SubjectVC: UIViewController {
     }
     
     func testBehaviorSub() {
-          
+        let lSub = BehaviorSubject.init(value: 999)
+        
+//        lSub.onNext(000)
+        
+        lSub.subscribe(onNext: {
+            obj in
+            print("1st订阅:",obj)
+        }, onError: { (err) in
+            print(err)
+        }, onCompleted: {
+            print("1st completed")
+        }) {
+            print("1st disposed")
+        }.disposed(by: ylDisBag)
+        
+        lSub.onNext(111)
+        
+        lSub.subscribe(onNext: {
+            obj in
+            print("2st订阅:",obj)
+        }, onError: { (err) in
+            print(err)
+        }, onCompleted: {
+            print("2st completed")
+        }) {
+            print("2st disposed")
+        }.disposed(by: ylDisBag)
+        
+        lSub.onNext(222)
+        
+        lSub.onCompleted()
+        
+        lSub.subscribe(onNext: {
+            obj in
+            print("3st订阅:",obj)
+        }, onError: { (err) in
+            print(err)
+        }, onCompleted: {
+            print("3st completed")
+        }) {
+            print("3st disposed")
+        }.disposed(by: ylDisBag)
+        
+        lSub.onNext(333)
+        
+        /*
+         log:
+         1st订阅: 999
+         1st订阅: 111
+         2st订阅: 111
+         1st订阅: 222
+         2st订阅: 222
+         1st completed
+         1st disposed
+         2st completed
+         2st disposed
+         3st completed
+         3st disposed
+         */
+    }
+    
+    func testBehaviorSubError(){
+        let lSub = BehaviorSubject.init(value: 999)
+        
+        //        lSub.onNext(000)
+        
+        lSub.subscribe(onNext: {
+            obj in
+            print("1st订阅:",obj)
+        }, onError: { (err) in
+            print(err)
+        }, onCompleted: {
+            print("1st completed")
+        }) {
+            print("1st disposed")
+        }.disposed(by: ylDisBag)
+        
+        lSub.onNext(111)
+        lSub.onError(YLError.someErr("test"))
+        
+        lSub.subscribe(onNext: {
+            obj in
+            print("2st订阅:",obj)
+        }, onError: { (err) in
+            print(err)
+        }, onCompleted: {
+            print("2st completed")
+        }) {
+            print("2st disposed")
+        }.disposed(by: ylDisBag)
+        
+        lSub.onNext(222)
+        
+        lSub.onCompleted()
+        
+        lSub.subscribe(onNext: {
+            obj in
+            print("3st订阅:",obj)
+        }, onError: { (err) in
+            print(err)
+        }, onCompleted: {
+            print("3st completed")
+        }) {
+            print("3st disposed")
+        }.disposed(by: ylDisBag)
+        
+        lSub.onNext(333)
+        
+        /*log:
+         1st订阅: 999
+         1st订阅: 111
+         someErr("test")
+         1st disposed
+         someErr("test")
+         2st disposed
+         someErr("test")
+         3st disposed
+         */
     }
     
     deinit{
