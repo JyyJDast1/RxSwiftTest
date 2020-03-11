8ea5714166722d613d1d80c3b72ea063d3bd50bf
Dast1 <myl90@126.com>
2020-03-11 14:15:16 +0800

    use ReplaySubject

--- RxSwiftBiliBili/RxSwiftBiliBili/Subject/SubjectVC.swift
+++ RxSwiftBiliBili/RxSwiftBiliBili/Subject/SubjectVC.swift
@@ -18,13 +18,15 @@ class SubjectVC: UIViewController {
         super.viewDidLoad()
         
         //无需初始值的Subject:类似直播
-//        testPublishSub()
+        //        testPublishSub()
         
         //需要初始值的Subject:订阅时可以收到订阅前上一次发送的消息（默认值底层也是消息，上一次无发送消息时，会收到默认值的消息）；
-//        testBehaviorSub()
+        //        testBehaviorSub()
         
-         //如果源 Observable 因为产生了一个 error 事件而中止， BehaviorSubject 就不会发出任何元素，而是将这个 error 事件发送出来。
-        testBehaviorSubError()
+        //如果源 Observable 因为产生了一个 error 事件而中止， BehaviorSubject 就不会发出任何元素，而是将这个 error 事件发送出来。
+        //        testBehaviorSubError()
+        
+        testReplaySub()
     }
     
     func testPublishSub() {
@@ -89,7 +91,7 @@ class SubjectVC: UIViewController {
     func testBehaviorSub() {
         let lSub = BehaviorSubject.init(value: 999)
         
-//        lSub.onNext(000)
+        //        lSub.onNext(000)
         
         lSub.subscribe(onNext: {
             obj in
@@ -207,6 +209,42 @@ class SubjectVC: UIViewController {
          */
     }
     
+    func testReplaySub(){
+        let lReSub = ReplaySubject<Int>.create(bufferSize: 2)
+        
+        lReSub.onNext(1)
+        lReSub.onNext(2)
+        lReSub.onNext(3)
+        
+        lReSub.subscribe { (event) in
+            print("1st ",event)
+        }.disposed(by: ylDisBag)
+        
+        lReSub.onNext(4)
+        
+        lReSub.subscribe { (event) in
+            print("2st ",event)
+        }.disposed(by: ylDisBag)
+        
+        lReSub.onCompleted()
+        
+        lReSub.subscribe { (event) in
+            print("3st ",event)
+        }.disposed(by: ylDisBag)
+        
+        /*log:
+         1st  next(2)
+         1st  next(3)
+         1st  next(4)
+         2st  next(3)
+         2st  next(4)
+         1st  completed
+         2st  completed
+         3st  next(3)
+         3st  next(4)
+         3st  completed
+         */
+    }
     deinit{
         print("vc dead")
     }
